262a7acab736bedaea5e598b9c5a4362c9510b7f
Mario Lopez <Mario.LopezHerdandez@student.csulb.edu>
2019-01-26 19:59:29 -0800

    added 404 page

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import Home from "./Components/Home/Home";
 import Login from "./Components/Login/Login";
+import NotFound from "./Components/NotFound/404";
 import AppliedRoute from "./AppliedRoute";
 
 import React from "react";
@@ -10,5 +11,7 @@ export default ({ childProps }) =>
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
+    { /* catch all unmatched routes */ }
+    <Route component={NotFound} />
   </Switch>;
 
